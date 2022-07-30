f0471a2858a7c99476b2f0f7974ab680c2b019c3
Ronaldo Melchiades Bicca <ronaldo@softkuka.com.br>
2022-07-30 10:44:11 -0300

    Now the create table

--- 01_docker/deasafios/02_node/nodeApp/index.js
+++ 01_docker/deasafios/02_node/nodeApp/index.js
@@ -1,32 +1,32 @@
 const express = require('express');
-const app = express();
 const port = 3000;
+const app = express();
 const config = {
     host: 'db',
     user: 'root',
     password: 'root',
-    database: 'nodedb'
+    database: 'nodedb',
+    multipleStatements: true
 };
 const mysql = require('mysql');
 const cn = mysql.createConnection(config);
 
-const sql = `insert into people(name) values ('Kuka o grande')`;
+const sql = `create table if not exists People(id int not null auto_increment, name varchar(255), primary key(id)); insert into people(name) values ('Ronaldo has been here!')`;
 cn.query(sql);
-//cn.end();
 
 app.get('/', async (req, res) => {
     const sql = `select * from people`;
-    await cn.query(sql, (err, result, fields) => {
+    await cn.query(sql, (err, result, afields) => {
         if (err) console.log(err);
         let regs = "<table><thread><tr><th>#Id</th><th>Nome</th></tr></thead><tbody>"
         for(var i=0; i < result.length; i++){
             regs = regs + '<tr><td>' + result[i].id + '</td><td>' + result[i].name + '</td></tr>';
         }
         regs = regs + '</tbody></table>'
-        res.send('<h1>Hello FullCycle from The Docker</h1><hr>' + regs.toString() );
+        res.send('<h1>Hello from Docker FullCycle</h1><hr>' + regs.toString() );
      });
 });
 
 app.listen(port, ()=> {
-    console.log('Rodando na porta ' + port);
+    console.log('Running on port ' + port);
 });
